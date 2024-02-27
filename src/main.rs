49b929bc9870393a340c1a2b1adb532c0c90dc87
Colin Huber <cbhuber@gmail.com>
2024-02-26 19:23:26 -0700

    test: Added initial unit tests

--- src/main.rs
+++ src/main.rs
@@ -57,3 +57,28 @@ fn solve_quadratic(a: f32, b: f32, c: f32) -> (f32, f32) {
     let root2: f32 = (-1.0 * b - d.sqrt()) / (2.0 * a);
     (root1, root2)
 }
+
+// UNIT TESTING -----------------------------------------------------------------
+
+#[cfg(test)]
+mod test {
+    // Everything above will be in scope for test
+    use super::*;
+
+    #[test]
+    fn test_valid_quadratic() {
+        assert_eq!(solve_quadratic(1.0, -7.0, 10.0), (5.0, 2.0));
+        assert_eq!(solve_quadratic(1.0, -1.0, -6.0), (3.0, -2.0));
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_invalid_quadratic() {
+        assert_eq!(solve_quadratic(0.0, -1.0, -6.0), (f32::INFINITY, f32::NAN));
+    }
+
+    #[test]
+    fn test_number() {
+        assert_eq!(check_number(&"5".to_string()), 5.0);
+    }
+}
