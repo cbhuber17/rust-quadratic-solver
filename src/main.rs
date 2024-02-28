e4446f7b9bf73275f16e8653e364ff03f6c4cd9b
Colin Huber <cbhuber@gmail.com>
2024-02-27 18:41:37 -0700

    test: Additional unit tests and tweaks

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 use std::process;
+// TODO: Use complex solutions for non-real roots
 
 fn main() {
     let a: f32 = get_input("a".to_string());
     if a == 0.0 {
         println!("Error: 'a' cannot be zero.");
+
+        #[cfg(not(test))]
         process::exit(1);
+
+        #[cfg(test)]
+        panic!();
     }
     let b: f32 = get_input("b".to_string());
     let c: f32 = get_input("c".to_string());
@@ -33,7 +39,12 @@ fn check_number(num_str: &String) -> f32 {
         Ok(number) => number,
         Err(_) => {
             println!("Error: {} is not a float. Exiting...", num_str.trim());
-            process::exit(1)
+
+            #[cfg(not(test))]
+            process::exit(1);
+
+            #[cfg(test)]
+            panic!();
         }
     }
 }
@@ -50,7 +61,7 @@ fn solve_quadratic(a: f32, b: f32, c: f32) -> (f32, f32) {
             "\nError:\na: {}\nb: {}\nc: {}\ndoes not have real roots.",
             a, b, c
         );
-        process::exit(1);
+        process::exit(0);
     }
 
     let root1: f32 = (-1.0 * b + d.sqrt()) / (2.0 * a);
@@ -69,6 +80,14 @@ mod test {
     fn test_valid_quadratic() {
         assert_eq!(solve_quadratic(1.0, -7.0, 10.0), (5.0, 2.0));
         assert_eq!(solve_quadratic(1.0, -1.0, -6.0), (3.0, -2.0));
+        assert_eq!(solve_quadratic(2.0, 5.0, -3.0), (0.5, -3.0));
+        assert_eq!(
+            solve_quadratic(-2.0, -3.0, 7.0),
+            (
+                (3.0 + f32::sqrt(65.0)) / -4.0,
+                (3.0 - f32::sqrt(65.0)) / -4.0
+            )
+        );
     }
 
     #[test]
@@ -80,5 +99,35 @@ mod test {
     #[test]
     fn test_number() {
         assert_eq!(check_number(&"5".to_string()), 5.0);
+        assert_eq!(check_number(&"5.".to_string()), 5.0);
+        assert_eq!(check_number(&"5.0".to_string()), 5.0);
+        assert_eq!(check_number(&"5.1".to_string()), 5.1);
+        assert_eq!(check_number(&"-1".to_string()), -1.0);
+        assert_eq!(check_number(&"-1.".to_string()), -1.0);
+        assert_eq!(check_number(&"-1.9".to_string()), -1.9);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_invalid_number1() {
+        assert_eq!(check_number(&"a".to_string()), 0.0);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_invalid_number2() {
+        assert_eq!(check_number(&" ".to_string()), 0.0);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_invalid_number3() {
+        assert_eq!(check_number(&"@".to_string()), 0.0);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_invalid_number4() {
+        assert_eq!(check_number(&"🙂".to_string()), 0.0);
     }
 }
